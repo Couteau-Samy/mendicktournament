906e5708baa307b770c596db0a909b483a735956
Couteau-Samy <68515378+Couteau-Samy@users.noreply.github.com>
2026-05-28 00:12:48 +0200

    Add files via upload

---
+++ data/players.ts
@@ -0,0 +1,32 @@
+export const players = [
+  {
+    name: "Mendick",
+    riotId: "xGc Ruler#GOAT",
+    // Exemple : S'il a commencé Or II avec 45 LP
+    startTier: "GOLD",   // En MAJUSCULES (IRON, BRONZE, SILVER, GOLD, PLATINUM, EMERALD, DIAMOND, MASTER)
+    startRank: "III",     // IV, III, II ou I
+    startLp: 25,
+    startWins: 31,    // <--- Nombre de victoires sur leur profil juste avant le 1er Juin
+    startLosses: 24,
+  },
+  {
+    name: "SamyDiCouti",
+    riotId: "xGc Keria#BEST",
+    // Exemple : S'il a commencé Émeraude IV avec 0 LP
+    startTier: "BRONZE",
+    startRank: "II",
+    startLp: 88,
+    startWins: 0,    // <--- Nombre de victoires sur leur profil juste avant le 1er Juin
+    startLosses: 6,
+  },
+  {
+    name: "Nulard de Greg",
+    riotId: "karbgreg#EUW",
+    // Exemple : S'il a commencé Argent I avec 85 LP
+    startTier: "BRONZE",
+    startRank: "I",
+    startLp: 27,
+    startWins: 19,    // <--- Nombre de victoires sur leur profil juste avant le 1er Juin
+    startLosses: 9,
+  },
+];
